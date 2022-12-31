152a6cfe9b66f2bd5e91007fd8ea5a3a72db88a4
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-31 18:03:13 +0530

    python-course

--- hello.py
+++ hello.py
@@ -6,3 +6,15 @@ print("Hello World")
 ''' Arye bhai hai kya yeh
 app kyu bta rhe ho yeh
 hmare kya kam ayega'''
+
+#print what is python?
+
+#print("Python is programing language which help us to communicate with computer or laptop even with any type of opreating system.
+#It is very simple and easy to understand  like reading a simple English.
+#Python pseudo code nature make python easy to learn for non-codder to just start learning coding and machine learning")
+
+#But you can't print full defination like this becase as mention in Readme.md file if want to tackle more than one line string than ypu have use this - ' -> ''' <-' .
+
+print('''Python is programing language which help us to communicate with computer or laptop even with any type of opreating system.
+It is very simple and easy to understand  like reading a simple English.
+Python pseudo code nature make python easy to learn for non-codder to just start learning coding and machine learning''')
