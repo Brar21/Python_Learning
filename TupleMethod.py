699a3cdfdc0d853c5d94325f1c20bde6fd95dff5
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-01 16:54:34 +0530

    python course

--- TupleMethod.py
+++ TupleMethod.py
@@ -6,6 +6,6 @@ t=(2,7,1,2,15,5,6,5,1,5,18,1,1,1)
 print(t.count(1)) #total time "1" is coming in tupple "t"
 # 5 number will come
 
-#to find first occurence of 1 index
+#To find first occurence of 1 index you can use .index()
 print(t.index(1))
 # 2nd index number where first 1 is stored
