e052a2903f725ed548493c93430ef2e2844e9d69
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-03 18:50:07 +0530

    Python Course

--- Dictionary.py
+++ Dictionary.py
@@ -10,3 +10,12 @@ print(myDict["age"])#print only age
 print(myDict["work"])#print only work
 print(myDict["isStillworking"])#print only working?
 print(myDict["Numbers"])#print only numbers
+
+ #name=key
+        #Varinder=value
+#  *Data-Types you can put in Dictionary*
+#  1. Interger("int")
+#  2. Boolen ("bol")
+#  3.  String-Literal("str")
+#  4. List("lst")
+#  5. Dictionary("dic")  
