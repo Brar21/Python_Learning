bc7bd48c9c6ca133157055706ae8fd9e4bc5b8b1
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-04 11:34:31 +0530

    Python Course

---
+++ Sets.py
@@ -0,0 +1,8 @@
+a={1,2,3,4,5,6,6}
+print(a)
+'''as i mention in note set i collection of non-repititive elements when you print this you can see
+only one  6 is printed second is ignore by default
+'''
+
+#for Print type when print(type(a)) you get "set"
+print(type(a))
