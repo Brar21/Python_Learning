add28368d5eb45bc72a4650cee828338db9ddd79
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-05 20:21:39 +0530

    Python course

--- ConditionalExperssion.py
+++ ConditionalExperssion.py
@@ -1,3 +1,13 @@
 #you will make a decission on base of conditions
 #condition full-fill hogi tabi woh am hoga
 #other wise say no to work
+
+#Normal example for starting
+
+a=10
+if(a>11):
+     print("Yes")
+elif(a==10):
+     print("Equal")
+else:
+    print("No never")
