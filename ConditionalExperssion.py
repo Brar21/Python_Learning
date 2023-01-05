74c9aba690fcd95c575249eeecbaef7ebfcef1f8
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-05 20:27:08 +0530

    Python course

--- ConditionalExperssion.py
+++ ConditionalExperssion.py
@@ -5,9 +5,9 @@
 #Normal example for starting
 
 a=10
-if(a>11):
+if(a>11): #false this condition 10 > 11 because never can be happen
      print("Yes")
-elif(a==11):
+elif(a==11):#false this condition 10 = 11 because never can be happen
      print("Equal")
-else:
+else: #if all of above condition are not pass then this will excute 
     print("No never")
