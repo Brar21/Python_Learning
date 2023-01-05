b39b0d0d73eead2ce678ebe532dd2c067f71acec
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-05 23:57:32 +0530

    Python course

--- ConditionalExperssion.py
+++ ConditionalExperssion.py
@@ -94,3 +94,19 @@ num1=int(input("Enter number1 \n"))
 num2=int(input("Enter number2 \n"))
 num3=int(input("Enter number3 \n"))
 num4=int(input("Enter number4 \n"))
+#find geratest from them
+
+if( num1>num4):
+    f1=num1
+else:
+    f1=num4
+
+if( num2>num3):
+    f2=num2
+else:
+    f2=num3
+
+if(f1>f2):
+        print(f1+" is greatest")
+else:
+        print(f2+" is greatest")
