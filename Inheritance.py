25231b7336a61b7b265c5e014e8e6c82682550f6
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-15 12:31:57 +0530

    Python Course

--- Inheritance.py
+++ Inheritance.py
@@ -1,1 +1,17 @@
 #inheritance
+
+class Emplyoee:
+    company="JavascriptFolks"
+
+    def showDetails(self):
+        print("Emplyoee Data")
+
+class Programers(Emplyoee):
+      language="Python"
+      def getlanguage(self):
+        print(f"this language is {self.language}")
+e=Emplyoee()
+e.showDetails()
+p=Programers()
+p.showDetails()     #it will print smae details of Emplyoee class
+#because Programers claa has now details so it will take from Parent or Base class value   
