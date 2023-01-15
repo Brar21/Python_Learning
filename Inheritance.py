f43244b6b416065b42b705ee04fd3de7e8d7d002
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-15 12:54:04 +0530

    Python Course

--- Inheritance.py
+++ Inheritance.py
@@ -18,3 +18,7 @@ p.showDetails()     #it will print smae details of Emplyoee class
 #if you print language of Programers class
 p.getlanguage()#you get "python" because language is present there
 print(p.company)#than it will take from parent or Base class because in programers class no company is there
+
+
+#Single Inheritance example is Above this line
+
