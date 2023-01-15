1f83cefefe1ec2bb3d509ea8248e49fee3ae314b
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-15 17:42:38 +0530

    Python course

---
+++ SuperMethods.py
@@ -0,0 +1,33 @@
+ 
+class FamilyTree:#parent class
+    surname="Brar"
+    Head="Ranbir Singh Brar"
+    def HeadName(self):
+        print(f"Head of {self.Head}")
+
+class TreeBranch(FamilyTree):#child class of Familytree and parent of Newfamilyhead
+    mainBranch="Parmjite kaur Brar"
+    number=5
+    def Children(self):
+        print(f"Childs in Family {self.number}")
+
+class NewFamilyHead(TreeBranch):#chlid class of Treerbanch clas
+    Headname="Gurinder Brar"
+
+    def NewFamilymembers(self):
+        print(f'{self.number-1} Family members')
+
+    def HeadName(self):
+        super().HeadName()
+        print(f"{self.Headname} is ne familyhead")        
+
+h=FamilyTree()
+Sh=TreeBranch()
+#Sh.number=5
+n=NewFamilyHead()
+print(n.surname)
+print(n.mainBranch)
+Sh.Children()
+n.NewFamilymembers()
+print(n.Headname)#this will print only child class headname
+n.HeadName()#this will print first main or parent class headname after its own headname
