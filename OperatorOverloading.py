1b827ed0742e457c562e48032cbdd45a5998343b
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-17 15:43:28 +0530

    Python course

---
+++ OperatorOverloading.py
@@ -0,0 +1,27 @@
+#operator_overloading using dunder methods
+
+class Number:
+    def __init__(self,num):
+        self.num= num
+    
+    def __add__(self,num2):#this is example of dunder methods
+        print("Let add")
+        return self.num+num2.num
+    
+    def __add__(self,num2):#this is example of dunder methods
+        print("Let minus")
+        return self.num-num2.num
+    
+    def __add__(self,num2):#this is example of dunder methods
+        print("Let add")
+        return self.num*num2.num
+    
+    def __add__(self,num2):#this is example of dunder methods
+        print("Let add")
+        return self.num/num2.num
+n1=Number(4)
+n2=Number(6)
+sum=n1+n2
+print(sum)
+    
+        
