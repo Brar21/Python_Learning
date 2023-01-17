61fe5abe7e4d229debf5ac82fcea1c661dc68d45
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-17 15:34:38 +0530

    Python course

--- PropertyDecorators.py
+++ PropertyDecorators.py
@@ -2,10 +2,19 @@
 # @property_decorators help you to take function as varible
 class Employee:
         Name="varinder"
-        Middle="Singh"
-        Last="Brar"
+        pocktmoney=5004
+        saving=5000
         @property
-        def name(self):
-           return self.Name+" "+self.Middle+" "+self.Last
+        def totalmoney(self):
+           return self.pocktmoney+self.saving
+
+        @totalmoney.setter   
+        def totalmoney(self,value):
+            self.saving=value-self.pocktmoney
 data=Employee()
-print(data.name)  #here you get value from function write function as proporty
+print(data.totalmoney)  #here you get value from function write function as proporty
+data.totalmoney=9999
+print(data.saving)
+print(data.totalmoney)
+#if you want to set return value according to your need
+ 
