24583aae64fb005142a6407bd6127eb5ce5f3130
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-17 16:22:21 +0530

    Python course

--- OperatorOverloading.py
+++ OperatorOverloading.py
@@ -8,20 +8,28 @@ class Number:
         print("Let add")
         return self.num+num2.num
     
-    def __add__(self,num2):#this is example of dunder methods
+    def __sub__(self,num2):#this is example of dunder methods
         print("Let minus")
         return self.num-num2.num
     
-    def __add__(self,num2):#this is example of dunder methods
+    def __mul__(self,num2):#this is example of dunder methods
         print("Let Multiply")
         return self.num*num2.num
     
-    def __add__(self,num2):#this is example of dunder methods
-        print("Let divide")
-        return self.num/num2.num
-n1=Number(4)
+    #def __div__(self,num2):#this is example of dunder methods
+    #    print("Let divide")
+    #    return self.num+num2.num / 2
+    
+n1=Number(12)
 n2=Number(6)
 sum=n1+n2
+multi=n1*n2
+minus=n1-n2
+#divide=n1+n2/Number(2)
 print(sum)
+print(minus)
+print(multi)
+#print(divide)
+
     
         
