65d9ce9742c7a56b2d67ec46c798dbae7226aaff
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-22 17:12:56 +0530

    Python Course

--- RandomGenrater.py
+++ RandomGenrater.py
@@ -1,3 +1,19 @@
 import random
 randomNumber=random.randint(0,10)
 print(randomNumber)
+userguess=None  #for define userguess we have to declare
+guesses=0
+while(userguess!=randomNumber):
+    userguess=int(input())
+    guesses+=1    
+    if(userguess==randomNumber):
+        print("Guess Correctly")
+    else:
+        if(userguess>randomNumber):
+            print("Sorry guess smaller number")
+        else:            
+            print("Sorry guess larger number")
+
+
+print(f"total guesses {guesses} time")
+        
