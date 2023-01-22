28e59821663d7b94f84180cdf94f8394938b23bb
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-22 15:54:49 +0530

    Python Course

--- Inheritance Practice/AnimalFilterationwithInheritance.py
+++ Inheritance Practice/AnimalFilterationwithInheritance.py
@@ -1,15 +1,21 @@
 #Multi-level Inheritance
 class Animal:
     animalType="mammal"
-class Pets:
-    color:"white"
-    legs:"four"
-    tail:"small"
-    bread:"Labraydor"
-class Dog:
+class Pets (Animal):
+    color="white"
+    legs="four"
+    tail="small"
+    bread="Labraydor"
+class Dog(Pets):
     @staticmethod
     def bark():
         print("Bow Bow!")        
 
+class DogDetails(Pets):
+    def DogData(self):
+        return self.color
 dog=Dog()
 dog.bark()
+data=DogDetails()
+#data.DogData()
+print(data.color)
